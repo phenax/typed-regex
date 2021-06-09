30ef482d1b7bc6c125de1b7ff87ca3b082c0e22c
Akshay Nair <phenax5@gmail.com>
2021-06-09 22:42:54 +0530

    minor refactor with class

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,18 @@ type RegExParser<Re extends string> =
 
 type RegExResult<Re extends string> = RegExParser<Re> | null;
 
-export const RegEx = <Re extends string>(re: Re, flags: string = '') => {
-  const regexp = new RegExp(re, flags);
-  return {
-    match: (str: string) => regexp.exec(str)?.groups as unknown as RegExResult<Re>,
-  };
-};
+class RegExT<Re extends string> {
+  regex : RegExp;
+
+  constructor(re: Re, flags: string = '') {
+    this.regex = new RegExp(re, flags);
+  }
+  
+  match(str: string) {
+    return this.regex.exec(str)?.groups as unknown as RegExResult<Re>;
+  }
+}
+
+export const RegEx = <Re extends string>(re: Re, flags: string = '') =>
+  new RegExT(re, flags);
 
